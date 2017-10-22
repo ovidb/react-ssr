976033b983b644f8e4c30a1fc8f9b2f16713b16e
Ovidiu D Bodea <antarexis@gmail.com>
2017-10-22 12:50:52 +0100

    Client bootup

--- src/client/client.js
+++ src/client/client.js
@@ -1,4 +1,9 @@
-//
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
 
-console.log('Setup is ok');
+// ReactDOM.render(<Home />, document.querySelector('#root'));
+// -> Calling ReactDOM.render() to hydrate server-rendered markup will stop working in React v17.
+// -> Replace the ReactDOM.render() call with ReactDOM.hydrate()
 
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
