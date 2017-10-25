2843f94b7e1b74d31023bfbb524f43b5cc9d18bb
Ovidiu D Bodea <antarexis@gmail.com>
2017-10-25 21:07:55 +0100

    `matchRoutes()` returns an array of the routes and respective components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
 import renderer from './helpers/rederer';
 import configureStore from './helpers/configureStore.dev';
 import rootSaga from './client/sagas';
+import Routes from './client/routes';
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = configureStore();
   store.runSaga(rootSaga);
+
+  console.log(matchRoutes(Routes, req.path));
 
   res.send(renderer(req, store));
 });
