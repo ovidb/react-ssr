00b2e025d90edc1bbc0ef7254aaccd6a6ec6f06c
Ovidiu D Bodea <antarexis@gmail.com>
2017-10-25 20:55:59 +0100

    react-router-config type routes

--- src/client/client.js
+++ src/client/client.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
 
 import Routes from './Routes';
 import configureStore from './store/configureStore.dev';
@@ -14,7 +15,7 @@ store.runSaga(rootSaga);
 ReactDOM.hydrate(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes />
+      <div>{renderRoutes(Routes)}</div>
     </BrowserRouter>
   </Provider>,
   document.querySelector('#root'),
